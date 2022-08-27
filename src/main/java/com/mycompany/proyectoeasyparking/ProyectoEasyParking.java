23e8e364795e09e61b6050d5807e301b11ac4349
ALIX NIDIA REYES <ALIX NIDIA REYES@WILMER-REYES>
2022-08-27 09:41:44 -0500

    Proyecto de clase ciclo 3 030Group17 (EASY PARKING) Primer commit

---
+++ src/main/java/com/mycompany/proyectoeasyparking/ProyectoEasyParking.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectoeasyparking;
+
+/**
+ *
+ * @author ALIX NIDIA REYES
+ */
+public class ProyectoEasyParking {
+
+    public static void main(String[] args) {
+     
+       
+        int n = 5;
+        int a =10;
+        System.out.println(n*a);
+   
+    }
+    
+  
+}
